8cde0d1457a1bdd7c03489743d699d0efb2a82f2
Laurent Debacker <debackerl gmail com>
2015-08-28 20:42:33 +0200

    Add comments to table columns; add trigger.

--- pgasus.sql
+++ pgasus.sql
@@ -1,121 +1,61 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 9.4.1
--- Dumped by pg_dump version 9.4.0
--- Started on 2015-04-11 00:58:53
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-
---
--- TOC entry 7 (class 2615 OID 16395)
--- Name: pgasus; Type: SCHEMA; Schema: -; Owner: postgres
---
-
 CREATE SCHEMA pgasus;
-
 
 SET search_path = pgasus, pg_catalog;
 
---
--- TOC entry 1864 (class 1247 OID 16598)
--- Name: http_method; Type: TYPE; Schema: pgasus; Owner: laurent
---
-
 CREATE TYPE http_method AS ENUM (
-    'get',
-    'post',
-    'put',
-    'patch',
-    'delete'
+	'get',
+	'post',
+	'put',
+	'patch',
+	'delete'
 );
-
-
---
--- TOC entry 1867 (class 1247 OID 16610)
--- Name: object_type; Type: TYPE; Schema: pgasus; Owner: laurent
---
 
 CREATE TYPE object_type AS ENUM (
-    'relation',
-    'procedure'
+	'relation',
+	'procedure'
 );
-
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
---
--- TOC entry 175 (class 1259 OID 16620)
--- Name: routes; Type: TABLE; Schema: pgasus; Owner: laurent; Tablespace: 
---
 
 CREATE TABLE routes (
-    route_id integer NOT NULL,
-    method http_method NOT NULL,
-    url_path text NOT NULL,
-    object_name text NOT NULL,
-    object_type object_type NOT NULL,
-    ttl integer DEFAULT 0 NOT NULL,
-    is_public boolean NOT NULL,
-    hidden_fields text[] DEFAULT ARRAY[]::text[] NOT NULL,
-    readonly_fields text[] DEFAULT ARRAY[]::text[] NOT NULL,
-    constants jsonb DEFAULT 'null'::jsonb NOT NULL,
-    context_mapped_headers public.hstore DEFAULT ''::public.hstore NOT NULL,
-    context_mapped_variables text[] DEFAULT ARRAY[]::text[] NOT NULL,
-    max_limit integer NOT NULL DEFAULT 0
+	route_id serial NOT NULL,
+	method admin.http_method NOT NULL,
+	url_path text NOT NULL, -- URL route, e.g. /users/:user_id/files/*path, where user_id and path are variables
+	object_name text NOT NULL, -- name of relation or procedure
+	object_type admin.object_type NOT NULL,
+	ttl integer NOT NULL DEFAULT 0, -- cache-control setting used for responses, in seconds
+	is_public boolean NOT NULL, -- cache-control setting used for responses
+	constants jsonb NOT NULL DEFAULT '{}'::jsonb, -- set of contstants to use in where clauses or as procedure arguments
+	context_mapped_headers hstore NOT NULL DEFAULT ''::hstore, -- context variables copied from HTTP header using specified mapping, where keys are HTTP header keys, and values are variable names
+	context_mapped_variables text[] NOT NULL DEFAULT ARRAY[]::text[], -- context variables copied from URL if available, or from cookie otherwise
+	max_limit integer NOT NULL DEFAULT 0, -- maximum number of records that can be requested when using a select statement
+	hidden_fields text[] NOT NULL DEFAULT ARRAY[]::text[], -- used for searchable fields which should not be displayed
+	readonly_fields text[] NOT NULL DEFAULT ARRAY[]::text[], -- fields that could not be saved via insert or update statements
+	CONSTRAINT rules_rule_id_pkey PRIMARY KEY (route_id)
 );
 
+COMMENT ON COLUMN routes.url_path IS 'URL route, e.g. /users/:user_id/files/*path, where user_id and path are variables';
+COMMENT ON COLUMN routes.object_name IS 'name of relation or procedure';
+COMMENT ON COLUMN routes.ttl IS 'cache-control setting used for responses, in seconds';
+COMMENT ON COLUMN routes.is_public IS 'cache-control setting used for responses';
+COMMENT ON COLUMN routes.constants IS 'set of contstants to use in where clauses or as procedure arguments';
+COMMENT ON COLUMN routes.context_mapped_headers IS 'context variables copied from HTTP header using specified mapping, where keys are HTTP header keys, and values are variable names';
+COMMENT ON COLUMN routes.context_mapped_variables IS 'context variables copied from URL if available, or from cookie otherwise';
+COMMENT ON COLUMN routes.max_limit IS 'maximum number of records that can be requested when using a select statement';
+COMMENT ON COLUMN routes.hidden_fields IS 'used for searchable fields which should not be displayed';
+COMMENT ON COLUMN routes.readonly_fields IS 'fields that could not be saved via insert or update statements';
 
---
--- TOC entry 174 (class 1259 OID 16618)
--- Name: routes_rule_id_seq; Type: SEQUENCE; Schema: pgasus; Owner: laurent
---
+CREATE OR REPLACE FUNCTION routes_notify_trigger()
+	RETURNS trigger AS
+$BODY$
+begin
+	NOTIFY "goml.reload_routes";
+	RETURN NULL;
+end
+$BODY$
+	LANGUAGE plpgsql VOLATILE
+	COST 100;
 
-CREATE SEQUENCE routes_rule_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- TOC entry 4270 (class 0 OID 0)
--- Dependencies: 174
--- Name: routes_rule_id_seq; Type: SEQUENCE OWNED BY; Schema: pgasus; Owner: laurent
---
-
-ALTER SEQUENCE routes_rule_id_seq OWNED BY routes.route_id;
-
-
---
--- TOC entry 4136 (class 2604 OID 16623)
--- Name: route_id; Type: DEFAULT; Schema: pgasus; Owner: laurent
---
-
-ALTER TABLE ONLY routes ALTER COLUMN route_id SET DEFAULT nextval('routes_rule_id_seq'::regclass);
-
-
---
--- TOC entry 4143 (class 2606 OID 16630)
--- Name: rules_rule_id_pkey; Type: CONSTRAINT; Schema: pgasus; Owner: laurent; Tablespace: 
---
-
-ALTER TABLE ONLY routes
-    ADD CONSTRAINT rules_rule_id_pkey PRIMARY KEY (route_id);
-
-
--- Completed on 2015-04-11 00:58:57
-
---
--- PostgreSQL database dump complete
---
-
+CREATE TRIGGER routes_updated_trigger
+	AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE
+	ON routes
+	FOR EACH STATEMENT
+	EXECUTE PROCEDURE routes_notify_trigger();
